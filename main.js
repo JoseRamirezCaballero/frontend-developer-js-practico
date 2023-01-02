df6130ca18c467c03f965a6a6033af0b729f125c
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-01-01 23:43:29 -0600

    merge product details to index

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerIconMenu = document.querySelector('#burguerIconMenu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
-
 function toggleDesktopMenu(){
     // Yo lo hice asi, version nuv
     // return desktopMenu.classList.contains('inactive') ?  desktopMenu.classList.remove('inactive') : desktopMenu.classList.add('inactive') 
     // Version buena
     desktopMenu.classList.toggle('inactive')
+    // aside.classList.add('inactive')
 }
 
-const burguerIconMenu = document.querySelector('#burguerIconMenu')
-const mobileMenu = document.querySelector('.mobile-menu')
-
-burguerIconMenu.addEventListener("click", toggleDesktopMobile)
-
-function toggleDesktopMobile(){
+burguerIconMenu.addEventListener("click", toggleMobileMenu)
+function toggleMobileMenu(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
 }
+
+menuCarritoIcon.addEventListener("click", toggleCarritoAside)
+function toggleCarritoAside(){
+    mobileMenu.classList.add('inactive')
+    aside.classList.toggle('inactive')
+}
+
